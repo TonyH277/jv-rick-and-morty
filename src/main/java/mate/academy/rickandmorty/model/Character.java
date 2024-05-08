34b346cca021db0ad9ddb96495951a94f2bf680c
TonyH277 <robhobs50@gmail.com>
2024-05-08 14:10:27 +0300

    remove custom deserializer

--- src/main/java/mate/academy/rickandmorty/model/Character.java
+++ src/main/java/mate/academy/rickandmorty/model/Character.java
@@ -1,6 +1,5 @@
 package mate.academy.rickandmorty.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,13 +8,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import mate.academy.rickandmorty.utils.CharacterDeserializer;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "characters")
-@JsonDeserialize(using = CharacterDeserializer.class)
 public class Character {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
